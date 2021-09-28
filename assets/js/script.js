245a6838fd2ee121f0af91e684b5682010f2422e
Kenneth Felix <kennf03@gmail.com>
2021-09-27 23:02:38 -0400

    finished most of the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,10 @@ var resultScreenEl = document.querySelector("#resultScreen");
 var highscoresEl = document.querySelector("#highscores");
 var timerEl = document.querySelector('#timeEl');
 var questiontxt = document.querySelector('#question');
+var resultText = document.querySelector('#resultText');
+var scoreText = document.querySelector('#scoreText');
+var initialInput = document.querySelector('#initialsInput');
+var leaderboardEl = document.querySelector('#leaderboards');
 
 var startbtn = document.querySelector('#startButton');
 var answer1btn = document.querySelector('#answer1');
@@ -14,7 +18,9 @@ var submitbtn = document.querySelector("#submitInput");
 var goBackbtn = document.querySelector("#goBack");
 var resetbtn = document.querySelector("#resetScore");
 
-var shuffledQuestion;
+var leaderboard;
+var guess;
+var score;
 var timerCount = 99;
 var questions = [{
     question: "_____ is a picture in which the flows of computational paths are depicted.",
@@ -34,6 +40,7 @@ var questions = [{
     correctAns: 3
 } ]
 var currentQuestion = 0;
+var shuffledQuestion = questions.sort(() => Math.random()-0.5);
 
 //start function
 function startGame() {
@@ -48,13 +55,28 @@ answer4btn.textContent = shuffledQuestion[0].answers[3];
 countdown();
 }
 
+function init() {
+    leaderboard = JSON.parse(localStorage.getItem("leaderboard"))
+
+    if (leaderboard === undefined) {
+        leaderboard = [{
+            highscore: 0,
+            initials: " "
+        }]
+
+        localStorage.setItem("leaderboard", JSON.stringify(leaderboard))
+    }
+    else {
+        makeList()}
+
+}
 // question timer function
 function countdown() {
 
     timer = setInterval(function() {
         timerCount--;
         timerEl.textContent = timerCount;
-        if (timerCount === 0) {
+        if (timerCount <= 0) {
           // Clears interval
           clearInterval(timer);
 
@@ -62,30 +84,90 @@ function countdown() {
       }, 1000);
 }
 // Ask question and changes question
-function nextQuestion() {}
+function nextQuestion() {
+
+
+    questiontxt.textContent = shuffledQuestion[currentQuestion].question;
+    answer1btn.textContent = shuffledQuestion[currentQuestion].answers[0];
+    answer2btn.textContent = shuffledQuestion[currentQuestion].answers[1];
+    answer3btn.textContent = shuffledQuestion[currentQuestion].answers[2];
+    answer4btn.textContent = shuffledQuestion[currentQuestion].answers[3];
+    resultText.style.visibility = "hidden";
+
+}
 
 //function to test to see if its right and give an answer based off it
-function answerResult() {
+function answerResult(event) {
+    guess = event.currentTarget.value;
+    if(guess == shuffledQuestion[currentQuestion].correctAns)
+    {
+        resultText.textContent = "Correct";
+        resultText.style.visibility = "visible";
+        currentQuestion++;
+        if(currentQuestion != shuffledQuestion.length) {
+            nextQuestion();
+        }
+        else {
+            resultText.style.visibility = "hidden";
+             score = timerCount;
+             timerCount = 0;
+             toScoreScreen();
+        }
+    }
+    else {
+        resultText.textContent = "Wrong";
+        resultText.style.visibility = "visible";
+        timerCount = timerCount - 10;
+    }
 
+}
+//function to get to the score screen after all questions are displayed
+function toScoreScreen() {
+    questionScreenEl.style.visibility = "hidden";
+    resultScreenEl.style.visibility = "visible";
+    scoreText.textContent = "You score is " + score;
 }
 
 //function to submit intials to highscore
 function submitScore() {
+    resultScreenEl.style.visibility = "hidden";
+    highscoresEl.style.visibility = "visible";
+    leaderboard.push(score, initialInput.value);
+    makeList();
+    localStorage.setItem("leaderboard", JSON.stringify(leaderboard));
 
 }
 
+// makes leaderboard list
+function makeList() {
+    leaderboardEl.children.clear;
+    if(leaderboard != null){
+        leaderboard.sort(function(a, b){return b - a});
+    
+        for (let i = 0;i < leaderboard.length;i++) {
+            var li = document.createElement('li');
+            li.textContent = leaderboard[i].initials + ": " + leaderboard[i].highscore;
+            leaderboardEl.append(li);
+        }
+    }
+}
 // function to reset highscore list 
 function resetScore() {
+    leaderboardEl.children.clear;
+    localStorage.clear();
 
 }
 
 //function to go back to the main screen
 function mainScreen() {
-
+    highscoresEl.style.visibility = 'hidden';
+    startScreenEl.style.visibility = "visible";
 }
 
-//event listeners
 
+
+//event listeners
+init();
 startbtn.addEventListener('click', startGame);
 answer1btn.addEventListener('click', answerResult);
 answer2btn.addEventListener('click', answerResult);
